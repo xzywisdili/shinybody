827d4f5cac1fa9c51bf81bdb1b2006a72f102242
Robert Norberg <Robert.Norberg@moffitt.org>
2024-12-12 12:48:41 -0500

    Add demo app

---
+++ inst/app/app.R
@@ -0,0 +1,37 @@
+library(shiny)
+library(shinybody)
+
+# read in data
+data_path <- system.file("data", "cancer_dataset.csv", package = "shinybody")
+df <- read.csv(data_path, header = TRUE)
+
+distinct_patients <- unique(df$sample_id)
+
+ui <- function() {
+  fluidPage(
+    selectInput(
+      inputId = "select_patient",
+      label = "Select Patient",
+      choices = distinct_patients,
+      multiple = FALSE,
+      selected = distinct_patients[1]
+    ),
+    humanOutput(outputId = "human"),
+    verbatimTextOutput(outputId = "debug")
+  )
+}
+
+server <- function(input, output) {
+  patient_rows <- reactive({
+    df[df$sample_id == input$select_patient,]
+  })
+  patient_cancer_msg <- reactive({
+    paste(
+      "Selected patient's cancer originates from",
+      paste(patient_rows$cancer_origin, collapse = ", ")
+    )
+  })
+  output$debug <- renderPrint(patient_cancer_msg)
+}
+
+shinyApp(ui = ui, server = server)
