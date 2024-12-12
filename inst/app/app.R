60acb86c631fc7d5d0ec1e2e4d25d35f4957828f
Robert Norberg <Robert.Norberg@moffitt.org>
2024-12-12 16:19:23 -0500

    use human widget in the demo app

--- inst/app/app.R
+++ inst/app/app.R
@@ -26,23 +26,31 @@ server <- function(input, output) {
   patient_rows <- reactive({
     df[df$sample_id == input$select_patient,]
   })
-  patient_cancer_msg <- reactive({
-    paste(
-      "Selected patient's cancer originates from",
-      paste(patient_rows()$cancer_origin, collapse = ", ")
-    )
-  })
 
   output$human <- renderHuman({
-    g <- unique(patient_rows()$gender)
-    if (g == "F") {
-      patient_gender <- "female"
-    } else {
-      patient_gender <- "male"
-    }
-    human(gender = patient_gender)
+    g <- ifelse(unique(patient_rows()$gender) == "F", "female", "male")
+    primary_tumor <- unique(patient_rows()$cancer_origin)
+    metastases <- unique(patient_rows()$metastasis_area)
+    colors <- c("black", rep("blue", length(metastases)))
+    names(colors) <- c(primary_tumor, metastases)
+    hovertext <- c("Primary Tumor", rep("Metastais", length(metastases)))
+    names(hovertext) <- c(primary_tumor, metastases)
+    human(
+      gender = g,
+      shown = c(primary_tumor, metastases),
+      selected = primary_tumor,
+      show_color = colors,
+      select_color = "red",
+      hovertext = hovertext
+    )
   })
-  output$debug <- renderPrint(patient_cancer_msg())
+  output$debug <- renderPrint({
+    primary_tumor <- unique(patient_rows()$cancer_origin)
+    paste(
+      "Selected patient's cancer originates from",
+      primary_tumor
+    )
+  })
   output$debug2 <- renderPrint(input$clicked_body_part)
 }
 
